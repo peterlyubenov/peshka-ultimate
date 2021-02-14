1509d6e07d557758002b23b72b8b640a0e75e774
Peter Lyubenov <peter.lyubenov98@gmail.com>
2021-02-14 10:59:40 +0200

    add themes to use with react-css-vars

---
+++ resources/react-app/src/themes/dark.theme.js
@@ -0,0 +1,7 @@
+const dark = {
+    componentBackgroundColor: "#292929",
+    textColor: "#ececec",
+    textColorLight: "#656565",
+};
+
+export default dark;
---
+++ resources/react-app/src/themes/light.theme.js
@@ -0,0 +1,6 @@
+const light = {
+    componentBackgroundColor: "#f8f8f8",
+    textColor: "#292929",
+    textColorLight: "#979797",
+};
+export default light;
