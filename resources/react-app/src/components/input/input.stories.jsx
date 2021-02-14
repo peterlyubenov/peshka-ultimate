9bcee1d37b97d5a77dd20d3f0cb5b11f7438acab
Peter Lyubenov <peter.lyubenov98@gmail.com>
2021-02-14 11:46:47 +0200

    removed the value from input story to unlock editing

--- resources/react-app/src/components/input/input.stories.jsx
+++ resources/react-app/src/components/input/input.stories.jsx
@@ -7,18 +7,17 @@ import light from "../../themes/light.theme";
 import dark from "../../themes/dark.theme";
 
 const template = ({ darkTheme, value }) => (
-    <ThemeSwitcher theme={darkTheme ? dark : light}>
-        <Input value={value}></Input>
-    </ThemeSwitcher>
+  <ThemeSwitcher theme={darkTheme ? dark : light}>
+    <Input></Input>
+  </ThemeSwitcher>
 );
 
 export const Default = template.bind({});
 Default.args = {
-    darkTheme: false,
-    value: "Hello world",
+  darkTheme: false,
 };
 
 export default {
-    title: "Components/Input",
-    component: Input,
+  title: "Components/Input",
+  component: Input,
 };
